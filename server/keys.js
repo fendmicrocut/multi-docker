1519f48d063634ede0853729151613da684e260c
Sharol Affandi <fendmicrocut@gmail.com>
2026-05-23 03:56:44 +0800

    Update PG variables

--- server/keys.js
+++ server/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
-  redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT,
+  redisHost: process.env.REDIS_HOST || '127.0.0.1',
+  redisPort: process.env.REDIS_PORT || 6379,
   pgUser: process.env.PGUSER || 'postgres',
   pgHost: process.env.PGHOST || '127.0.0.1',
   pgDatabase: process.env.PGDATABASE || 'postgres',
-  pgPassword: process.env.PGPASSWORD || 'postgres_password',
+  pgPassword: process.env.PGPASSWORD,
   pgPort: process.env.PGPORT || 5432,
 };
